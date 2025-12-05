d65cabe9b9b0ec21d2cf071b2bcfec81617a728f
hugo rath <hugo.rath@epitech.eu>
2025-12-05 16:05:31 +0100

    added mesure feature

---
+++ ArboreUi/ArboreUi/measure app/measure_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  measure_appApp.swift
+//  measure app
+//
+//  Created by hugo rath on 05/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct measure_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

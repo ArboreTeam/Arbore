619fc81d014f638addc4f03bd7c3c3a88a7e5131
hugo rath <hugo.rath@epitech.eu>
2026-01-05 15:31:33 +0100

    feat : added possibility to move and resize the plant when in the AR Garden and possibility to get back too

--- ArboreUi/ArboreUi/LoginAuth/Extensions.swift
+++ ArboreUi/ArboreUi/LoginAuth/Extensions.swift
@@ -28,3 +28,16 @@ extension Color {
         self.init(red: r, green: g, blue: b)
     }
 }
+
+extension UIColor {
+    convenience init(hex: String) {
+        let scanner = Scanner(string: hex)
+        _ = scanner.scanString("#")
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+        let r = CGFloat((rgb >> 16) & 0xFF) / 255
+        let g = CGFloat((rgb >> 8) & 0xFF) / 255
+        let b = CGFloat(rgb & 0xFF) / 255
+        self.init(red: r, green: g, blue: b, alpha: 1.0)
+    }
+}

5205c4a9a9f07d8c7e4829342822cd8d4d8dee6d
hugo rath <hugo.rath@epitech.eu>
2026-01-12 19:45:15 +0100

    feat : fix : fixed the saveing of garden plants

--- ArboreUi/ArboreUi/Thumbnail/PlantThumbnailCache.swift
+++ ArboreUi/ArboreUi/Thumbnail/PlantThumbnailCache.swift
@@ -14,7 +14,7 @@ struct PlantThumbnailCache {
         return dir
     }
 
-    private static let version = "v4"
+    private static let version = "v5"
 
     static func url(for plantID: String) -> URL {
         directory.appendingPathComponent("\(plantID)_\(version).png")
